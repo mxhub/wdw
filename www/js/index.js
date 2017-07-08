0dafcc1c429942e5ada2b0f19ca199e249646e97
mxhub <joseph@mxhub.com>
2017-07-08 14:47:29 +0800

    Add files via upload

--- www/js/index.js
+++ www/js/index.js
@@ -38,7 +38,7 @@ var app = {
 		if (navigator.connection.type == Connection.NONE) {
       navigator.notification.alert('An internet connection is required to continue');
     } else {
-      window.location="http://www.google.com";
+      window.location="https://www.oowdwoo.com";
     }
 
     },
